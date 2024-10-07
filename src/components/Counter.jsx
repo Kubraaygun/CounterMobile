1725de1983a94a63d0afa6275fe0e0e6a84d63d8
¨kubraaygun¨ <kubraaygun@outlook.com.tr>
2024-10-07 17:17:24 +0300

    duzenleme yapildi

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,16 +1,53 @@
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import {useEffect, useState} from 'react';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import {useDebugValue, useEffect, useState} from 'react';
 
 const Counter = () => {
-  const [count, setCount] = useState(10);
+  const [count, setCount] = useState(0);
 
-  //1) Bilesenin ekrana basilma olayini izle
+  const [inputValue, setInputValue] = useState('');
+
+  /*   //1) Bilesenin ekrana basilma olayini izle
   useEffect(() => {
     console.log('bilesen ekrana basildi');
   }, []);
 
+  //2) Bilesen ekrandan gitme olayi
+  useEffect(() => {
+    return () => console.log('ekrandan gitti');
+  }, []); */
+
+  // //3) Bilesenin ekrana gelme ve gitme olayini izle
+  // useEffect(() => {
+  //   console.log('Bilesen ekrana geldi');
+  //   return () => console.log('Bilesen ekrandan gitti');
+  // }, []);
+
+  // //4) Bilesenin guncellenme UPDATE olma olayini izle
+  // useEffect(() => {
+  //   console.log('Bilesen RENDER OLDU STATE VEYA PROPS DEGISTI');
+  // });
+
+  //5) Input Degisimine bagimli olan degeri izle
+
+  useEffect(() => {
+    console.log('Input degeri degisti', inputValue);
+  }, [inputValue]);
+
   return (
     <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Değer Giriniz"
+        value={inputValue}
+        onChangeText={setInputValue}
+      />
+
       <TouchableOpacity
         onPress={() => setCount(count - 1)}
         disabled={count === 0}
@@ -65,5 +102,14 @@ const styles = StyleSheet.create({
     fontSize: 60,
     fontWeight: 'bold',
     marginVertical: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: 'purple',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 20,
+    width: '80%',
   },
 });
