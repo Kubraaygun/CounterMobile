03b08c137becf0920d4e1c0986a9025f3f5492c7
¨kubraaygun¨ <kubraaygun@outlook.com.tr>
2024-10-07 10:08:06 +0300

    Button tasarlandi

--- App.tsx
+++
@@ -1,9 +0,0 @@
-import {View, Text} from 'react-native';
-import React from 'react';
-import Counter from './src/components/Counter';
-
-const App = () => {
-  return <Counter />;
-};
-
-export default App;
